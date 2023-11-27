c002cf7744d2eebb4766779dc49823dd3cc84597
zerkvii <934991794@qq.com>
2023-11-28 01:03:45 +0800

    fix type error

--- src/main/java/com/zkvii/fu/Dao/UserDao.java
+++ src/main/java/com/zkvii/fu/Dao/UserDao.java
@@ -12,18 +12,18 @@ import com.zkvii.fu.Pojo.User;
 @Mapper
 public interface UserDao {
 
-    @Select("SELECT * FROM users WHERE name = #{name}")
+    @Select("SELECT * FROM Users WHERE name = #{name}")
     User getUserByName(String name);
 
     @Select("SELECT * FROM Users")
     List<User> getAllUsers();
 
-    @Insert("INSERT INTO Users (name, password, created_at, updated_at, privelege_level)" +
+    @Insert("INSERT INTO Users (name, password, created_at, updated_at, privilege_level)" +
             "VALUES (#{name}, #{password}, #{created_at}, #{updated_at}, #{privelege_level})")
     void addUser(User user);
 
     @Update("UPDATE Users SET name = #{name}, password = #{password},created_at = #{created_at}," +
-            "updated_at = #{updated_at}, privelege_level = #{privelege_level} WHERE name = #{name}")
+            "updated_at = #{updated_at}, privilege_level = #{privelege_level} WHERE name = #{name}")
     void updateUser(User user);
 
     @Update("DELETE FROM Users WHERE name = #{name}")
