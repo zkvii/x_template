f6d6ae93689ee3c6e0139825faf3736c59742043
root <root@rpc>
2023-11-27 20:57:06 +0800

    add ui for all

--- src/main/java/com/zkvii/fu/Controller/EchoController.java
+++ src/main/java/com/zkvii/fu/Controller/EchoController.java
@@ -1,20 +1,22 @@
 package com.zkvii.fu.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.zkvii.fu.Dao.UserDao;
 
-@RestController
-@RequestMapping("/echo")
+@Controller
 public class EchoController {
-    @Autowired
-    private UserDao userDao;
+    // @Autowired
+    // private UserDao userDao;
 
-    @GetMapping("")
-    public String echo() {
-        return userDao.getAllUsers().toString();
+    // @GetMapping("")
+    // public String echo() {
+    //     return userDao.getAllUsers().toString();
+    // }
+    @GetMapping("/echo")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello, Thymeleaf!");
+        return "echo"; // This corresponds to the template file name without the extension
     }
 }
